b087a899dbdfb6ff2a7629bcbecb89793b243150
Suhan Mihai-Andrei <suhan.mihai.r4q@student.ucv.ro>
2025-09-21 11:33:56 +0300

    Solution

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer  = require('multer');
+const upload = multer();
 
 var app = express();
 
@@ -9,6 +11,19 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const file = req.file;
+  if (!file) {
+    return res.status(400).send('No file uploaded');
+  }
+  const fileInfo = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  };
+  res.json(fileInfo);
 });
 
 
